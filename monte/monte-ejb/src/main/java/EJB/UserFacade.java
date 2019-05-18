65ac9af3e673d2a6298a0aa26f78f770c2219fd4
Pecaalta <pecaalta@gmail.com>
2019-05-18 20:12:42 -0300

    fdsf

--- monte/monte-ejb/src/main/java/EJB/UserFacade.java
+++ monte/monte-ejb/src/main/java/EJB/UserFacade.java
@@ -6,9 +6,11 @@
 package EJB;
 
 import entities.User;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,5 +30,92 @@ public class UserFacade extends AbstractFacade<User> implements UserFacadeLocal 
     public UserFacade() {
         super(User.class);
     }
+    @Override
+    public User iniciarSesion(User c) {
+        User cliente = null;
+        String consulta;
+        try {
+            consulta = "FROM Cliente c WHERE c.email = ?1 and c.password = ?2";
+            Query query = em.createQuery(consulta);
+            query.setParameter(1, c.getEmail());
+            query.setParameter(2, c.getPass());
+            List<User> lista = query.getResultList();
+            if(!lista.isEmpty()){
+                cliente = lista.get(0);
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        return cliente;
+    }
+    
+    @Override
+    public User iniciarSesionRedes(String email){
+        User cliente = null;
+        String consulta;
+        try {
+            consulta = "FROM Cliente c WHERE c.email = ?1";
+            Query query = em.createQuery(consulta);
+            query.setParameter(1, email);
+            List<User> lista = query.getResultList();
+            if(!lista.isEmpty()){
+                cliente = lista.get(0);
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        return cliente;
+    }
+
+    @Override
+    public User findClienteByEmail(String email) {
+        User cliente = null;
+        String consulta = "FROM Cliente c WHERE c.email = ?1";
+        try{
+            Query query = em.createQuery(consulta);
+            query.setParameter(1, email);
+            List<User> lista = query.getResultList();
+                if(!lista.isEmpty()){
+                    cliente = lista.get(0);
+                }
+        } catch (Exception e) {
+            throw e;
+        }
+        return cliente;
+    }
+
+    @Override
+    public User usuarioDeGoogle() {
+        User cliente = null;
+        String consulta = "FROM Cliente c WHERE c.googleActivo = ?1";
+        try{
+            Query query = em.createQuery(consulta);
+            query.setParameter(1, "activo");
+            List<User> lista = query.getResultList();
+                if(!lista.isEmpty()){
+                    cliente = lista.get(0);
+                }
+        } catch (Exception e) {
+            throw e;
+        }
+        return cliente;
+    }
+    
+    @Override
+    public void usuarioDeGoogleCambiarEstado(User c){
+        
+
+        String consulta = "UPDATE Cliente SET apellido = 'alamierda' "
+                + "WHERE apellido = ?1";
+        try{
+            Query query = em.createQuery(consulta);
+            query.setParameter(1, "Montelongo");
+            int update = query.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        }
+        
+    }
+    
     
 }
